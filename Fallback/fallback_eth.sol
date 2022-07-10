7737d2d3072682e04585f76e46eb1caea4d1f590
tracy <zhangyanling1998@126.com>
2022-07-10 23:02:51 +0800

    replace content in file

--- Fallback/fallback_eth.sol
+++ Fallback/fallback_eth.sol
@@ -1,46 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-
-//fallback
+ 
+// 包含fallback函数的合约，合约账户能够接收到其它合约转的以太
 contract TestFallback {
     string message;
-
-    //构造函数，初始化状态变量message
-    constructor() {
+ 
+    //构造函数，初始化状态变量message，同时可向合约账户存款
+    constructor() payable {
         message = "hello";
     }
-
-    fallback() external {
-        message = "fallback"; 
+ 
+    //回退函数，能够为此合约账户接收以太
+    fallback() external payable {
     }
-
-    //调用此合约中不存在的函数
-    function testFallback() external returns (bytes memory) {
-
-        // 调用不存在的函数getMsgNew()
-        bytes memory method = abi.encodeWithSignature("getMsgNew()");
-        (bool success, bytes memory returnData) = address(this).call(method);
-
-        require(success, "get fail");
-        return returnData;
+ 
+    //存款，若部署时忘记存款，可直接调用此函数向合约账户存款
+    function deposit() external payable {
     }
-
-    //调用此合约中已存在函数，但是没有传递参数
-    function testFallbackWithNoParam() external returns (bytes memory) {
-
-        // 调用已存在的函数setMsg()，未传递参数
-        bytes memory method = abi.encodeWithSignature("setMsg()");
-        (bool success, bytes memory returnData) = address(this).call(method);
-
-        require(success, "set fail");
-        return returnData;
+ 
+    //发送以太
+    function sendEther(address _addr) external {
+        bool result = payable(_addr).send(2);
+        require(result, "send fail");
     }
-
-    function getMsg() external view returns (string memory) {
-        return message;
+    
+    //查看合约账户余额
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
     }
-
-    function setMsg(string memory _message) external {
-        message = _message;
+}
+ 
+ 
+// 不包含fallback函数的合约
+contract TestWithoutFallback {
+ 
+    //构造函数，初始化时可向合约账户存款
+    constructor() payable{
+    }
+ 
+    //存款，若部署时忘记存款，可直接调用此函数向合约账户存款
+    function deposit() external payable {
+    }
+ 
+    //发送以太
+    function sendEther(address _addr) external returns (bool) {
+        bool result = payable(_addr).send(2);
+        return result;
+    }
+ 
+    //查看合约账户余额
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 }
